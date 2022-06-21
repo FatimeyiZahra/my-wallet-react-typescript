3ca0a6d414021eeaaff0fe9c8cfafb3852f8750b
FatimeyiZahra <fatimeyizahrai@gmail.com>
2022-06-21 15:52:55 +0400

    userAction=> thunkAction

--- src/redux/actions/userActions.ts
+++ src/redux/actions/userActions.ts
@@ -1,8 +1,11 @@
-import { LoginForm, User, UserDispatch,LOGIN_ERROR,LOGIN_START,LOGIN_SUCCESS,UserAction } from "../../types/user";
+import { LoginForm, User, UserDispatch,LOGIN_ERROR,LOGIN_START,LOGIN_SUCCESS,UserAction, UserState } from "../../types/user";
 import api from "../../utils/api";
 import { Dispatch } from "redux";
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { RootState } from "../store/store";
 
-export const login = (creds: LoginForm) => async (dispatch: Dispatch<UserAction>) => {
+export const login = (creds: LoginForm): ThunkAction<void, RootState, unknown,  UserAction> =>
+async dispatch => {
   dispatch({ type: LOGIN_START });
   try {
     const response = await api.post<User>("/users/login", creds);
