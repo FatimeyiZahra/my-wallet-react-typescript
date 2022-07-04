7c96b9b347f73a771c068037c04f1161af7f472a
FatimeyiZahra <fatimeyizahrai@gmail.com>
2022-07-04 21:38:22 +0400

    modal 30%

--- src/components/CategoryPage.tsx
+++ src/components/CategoryPage.tsx
@@ -1,17 +1,41 @@
-import React, { useEffect } from "react";
-import { Space, Table, Tag } from "antd";
+import React, { useEffect, useState } from "react";
+import { Button, Form, Input, Modal, Select, Space, Table, Tag } from "antd";
 import { Category } from "../types/category";
 import { useAppDispatch, useAppSelector } from "../redux/hook";
 import { RootState } from "../redux/store/store";
 import { getAllCategory } from "../redux/actions/categoryActions";
 
+type Mode = "new" | "edit";
+
 const CategoryPage = () => {
-const data = useAppSelector((state:RootState)=>state.category)
-console.log(data)
-const dispatch= useAppDispatch()
-useEffect(() => {
- dispatch(getAllCategory())
-}, [])
+  const { data, loading, error } = useAppSelector(
+    (state: RootState) => state.category
+  );
+  console.log(data);
+
+  const dispatch = useAppDispatch();
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [mode, setMode] = useState<Mode>("new");
+
+  const showModal = (mode: Mode) => {
+    setIsModalVisible(true);
+    setMode(mode);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+    setMode("new");
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    setMode("new");
+  };
+
+  useEffect(() => {
+    dispatch(getAllCategory());
+  }, []);
 
   const columns = [
     {
@@ -41,7 +65,33 @@ useEffect(() => {
 
   return (
     <>
-      {/* <Table columns={columns} dataSource={data} /> */}
+      <div style={{ marginTop: "25px" }}>
+        <Button type="primary" onClick={() => showModal("new")}>
+          Create Category
+        </Button>
+        <Table columns={columns} dataSource={data} />
+      </div>
+      <Modal
+        title={mode === "new" ? "CREATE NEW CATEGORY" : "UPDATE CATEGORY"}
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Form labelCol={{ span: 4 }} wrapperCol={{ span: 14 }}>
+          <Form.Item label="category name">
+            <Input />
+          </Form.Item>
+          <Form.Item label="type">
+            <Select>
+              <Select.Option value="demo">Demo</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="Button">
+            <Button>Button</Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </>
   );
 };
